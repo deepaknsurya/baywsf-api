f57cc152eee8a558035ce3cc2b5207283456d537
Deepak N Surya <deepaknsurya7@gmail.com>
2024-10-23 02:39:40 -0700

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,6 +1,8 @@
 import mongoose from 'mongoose';
 import { seedDoctorDatabase } from './models/Doctor.model';
 
+// PORT=3000
+// MONGO_URI=mongodb+srv://deepaknsurya7:L74tIVV187lENDBf@cluster0abaywsf.op1pm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0abaywsf
 const connectDB = async () => {
     try {
         const conn = await mongoose.connect(process.env.MONGO_URI as string);
